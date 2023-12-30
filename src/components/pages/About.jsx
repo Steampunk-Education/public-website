c9b39d6fbc5ba264514b1c5b822dd202d21892f1
thePianoKid <gbraden.dev@gmail.com>
2023-12-29 22:18:53 -0500

    Made about section mobile-friendly.

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -6,6 +6,8 @@ import ThemedButton from "../ThemedButton";
 import { properties } from "../../properties";
 import aboutUsImage from "../../resources/images/service3-img.jpeg";
 import { Link } from "react-router-dom";
+import AboutText from "../AboutText";
+import Box from "@mui/material/Box";
 
 const AboutTitle = styled(Typography)(() => ({
   margin: "auto",
@@ -27,37 +29,28 @@ const FullHeightGrid = styled(Grid)(() => ({
 function About() {
   return (
     <div id="aboutAnchor">
-      <AboutTitle sx={{ pt: 18, pb: 5 }}>What is Steampunk?</AboutTitle>
-      <FullHeightGrid
-        container
-        justify="flex-end"
-        alignItems="center"
-        spacing={0}
-      >
-        <Grid xs={5}>
-          <img
-            style={{ width: "100%" }}
-            src={aboutUsImage}
-            alt="Girl looking at user; artistic equations decorate the frame"
-          />
-        </Grid>
-        <Grid xs={7} sx={{ pr: 10 }}>
-          <SplashPageBody>
-            Steampunk's goal is to show young creatives how to harness the
-            incredible power of technology to enable them to create, instead of
-            consume.
-          </SplashPageBody>
-          <br />
-          <SplashPageBody>
-            We teach students how to get started in robotics, programming, 3D design & AI. 
-            At Steampunk, we believe that an understanding of computer science and basic engineering principles are as vital as reading and arithmetic in this modern age. 
-          </SplashPageBody>
-          <Container sx={{ pb: "30px" }}></Container>
-          <Link to="/quiz?usertype=educator&lang=en">
-            <ThemedButton btnText={"Educators, start here"} />
-          </Link>
-        </Grid>
-      </FullHeightGrid>
+      <Box sx={{ display: { xs: "none", md: "block" } }}>
+        <FullHeightGrid
+          container
+          justify="flex-end"
+          alignItems="center"
+          spacing={0}
+        >
+          <Grid xs={5}>
+            <img
+              style={{ width: "100%" }}
+              src={aboutUsImage}
+              alt="Girl looking at user; artistic equations decorate the frame"
+            />
+          </Grid>
+          <Grid xs={7} sx={{ pr: 10 }}>
+            <AboutText />
+          </Grid>
+        </FullHeightGrid>
+      </Box>
+      <Box sx={{ display: { xs: "block", md: "none" }, pt: 10, pl: 5, pr: 5  }}>
+        <AboutText />
+      </Box>
     </div>
   );
 }
