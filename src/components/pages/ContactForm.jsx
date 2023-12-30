c259d7cd7b157c38d66ba96b34e11c96a5e6468f
thePianoKid <gbraden.dev@gmail.com>
2023-12-30 15:31:21 -0500

    Added emailjs code - creds missing

--- src/components/pages/ContactForm.jsx
+++ src/components/pages/ContactForm.jsx
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
-import { Typography, TextField, Grid, ListItem } from "@mui/material";
+import React, { useRef, useState } from "react";
+import { Typography, TextField, Grid } from "@mui/material";
 import { styled } from "@mui/material/styles";
 import { properties } from "../../properties";
 import ThemedButton from "../ThemedButton";
+import emailjs from "emailjs-com";
 
 const SplashPageTitle = styled(Typography)(() => ({
   fontFamily: "Josefin Sans",
@@ -15,12 +16,6 @@ const SplashPageBody = styled(Typography)(() => ({
   color: properties.style.primary.grey,
 }));
 
-const StyledListItem = styled(ListItem)(() => ({
-  fontFamily: "Roboto",
-  color: properties.style.primary.grey,
-  fontSize: "16px",
-}));
-
 function ContactForm(props) {
   const { titleText, submitBtnText, requireMessage } = props;
 
@@ -29,6 +24,9 @@ function ContactForm(props) {
   const [message, setMessage] = useState("");
   const [emailError, setEmailError] = useState(false);
   const [messageError, setMessageError] = useState(false);
+  const [emailjsError, setEmailjsError] = useState(false);
+
+  const form = useRef();
 
   const handleEmailChange = (event) => {
     setEmail(event.target.value);
@@ -38,22 +36,28 @@ function ContactForm(props) {
     setMessage(event.target.value);
   };
 
-  const handleClick = () => {
+  const handleClick = (e) => {
+    e.preventDefault();
     setEmailError(false);
     setMessageError(false);
 
-    console.log(requireMessage)
+    console.log(e)
 
     if (email.length > 0) {
       if (email.includes("@")) {
         const splitEmail = email.split("@");
         if (splitEmail[1].includes(".")) {
             if (requireMessage && message.length <= 0) {
-                setMessageError(true);
-                return;
+              setMessageError(true);
+              return;
             } else {
-                setConfirmation(true);
-                return;
+              setConfirmation(true);
+              emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_USER_ID')
+                .then(() => {
+                }, (error) => {
+                  console.log(error)
+                  setEmailjsError(true);
+                })
             }
         }
       }
@@ -70,22 +74,16 @@ function ContactForm(props) {
     <div>
       {confimation ? (
         <div>
-          <SplashPageTitle sx={{ pt: 4, ml: 3 }}>Success!</SplashPageTitle>
+          <SplashPageTitle sx={{ pt: 4, ml: 3 }}>{emailjsError ? "Something went wrong." : "Success!"}</SplashPageTitle>
           <SplashPageBody sx={{ pt: 2, pb: 1, ml: 3 }}>
-            Thank you for your interest. We will get back to you in 1-2 business
-            days.
+            {emailjsError ? 
+            "Your message was not sent by the system, please reach out to the team directly via info@steampunk.education."
+            : "Thanks for reaching out! We look forward to working with you."
+            }
           </SplashPageBody>
-
-          <SplashPageBody sx={{ pt: 2, pb: 1, ml: 3 }}>
-            Submission details
-          </SplashPageBody>
-          <StyledListItem sx={{ ml: 3 }}>Email: {email}</StyledListItem>
-          <StyledListItem sx={{ ml: 3 }}>
-            Other comments: {message}
-          </StyledListItem>
         </div>
       ) : (
-        <div>
+        <form ref={form}>
           <SplashPageTitle sx={{ pt: 4, pb: 2, ml: 3 }}>
             {titleText}
           </SplashPageTitle>
@@ -93,7 +91,7 @@ function ContactForm(props) {
             <TextField
               sx={{ ml: 3, width: 400 }}
               label="Enter your work email..."
-              variant="filled"
+              variant="outlined"
               onChange={handleEmailChange}
               error={emailError}
               helperText={emailError ? "Please enter a valid email." : ""}
@@ -103,7 +101,7 @@ function ContactForm(props) {
             <TextField
               sx={{ ml: 3, width: 600 }}
               label="Questions or comments?"
-              variant="filled"
+              variant="outlined"
               multiline
               rows={5}
               onChange={handleMessageChange}
@@ -114,9 +112,9 @@ function ContactForm(props) {
           <ThemedButton
             sx={{ ml: 3 }}
             btnText={submitBtnText}
-            onClick={handleClick}
+            onClick={(e) => handleClick(e)}
           />
-        </div>
+        </form>
       )}
     </div>
   );
